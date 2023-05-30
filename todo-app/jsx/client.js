52e26927696fb057bfc87b4fc61f1baa27e4245e
Derrick Schwabe <drschwabe@fastmail.com>
2023-05-30 16:38:42 -0600

    JSX todo app

---
+++ todo-app/jsx/client.js
@@ -0,0 +1,56 @@
+/** @jsx h *//** @jsxFrag h */
+const { log } = console
+const { html, render } = require('uhtml/cjs/x.js') 
+const { createPragma} = require('jsx2tag') 
+const h = createPragma(html)
+
+log('hello world') 
+
+const tasks = [] 
+let adding 
+
+const template = () => (
+  <div>
+    <ul>
+      { tasks.map( task => (
+        <li class="py-2 border-b border-gray-200">
+          {task}
+        </li> )
+      )}  
+    </ul>
+
+    { adding ? (
+        <div class="flex my-2">
+          <input class="mr-2" placeholder="new task" />  
+          <button id="save" onclick={saveTask}>save</button>
+        </div>
+      ) : null 
+    }
+
+    <button class={(adding ? 'opacity-50' : '' ) + `mt-4`}
+      onclick={enterTask}>
+      add task
+    </button>     
+  </div> 
+) 
+
+const doRender = () => render(document.getElementById('tasks'), 
+  template)    
+
+const enterTask = () => {
+  log('clicked add task') 
+  adding = true 
+  doRender() 
+  document.querySelector('input').focus() 
+}
+
+const saveTask = () => {
+  tasks.push( document.querySelector('input').value ) 
+  log(tasks)                    
+  adding = false
+  doRender() 
+} 
+
+
+doRender() 
+
---
+++ todo-app/jsx/server.js
@@ -0,0 +1,28 @@
+const { log } = console
+
+//Express: 
+const no = require('node-html')                 
+no.server(8001)
+no.static() 
+const { expressApp } = no 
+
+const html = `<html>
+<head>
+  ${no.twLocalScript}
+  ${no.twBasicStyle}
+</head>          
+<body class="p-6">
+
+  <h1 class="mb-3">todo app: <b>JSX</b></h1> 
+
+  <div id="tasks"></div> 
+
+  <script src="client.bundle.js"></script>
+
+</body>
+</html>`
+
+
+expressApp.get('/', (req, res) => {
+  res.send(html) 
+}) 
